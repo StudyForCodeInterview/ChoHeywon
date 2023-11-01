4f2ebb4573505ce23301f9a6f13613b0dbf939b2
chw-owo <96677719+chw-owo@users.noreply.github.com>
2023-11-02 02:05:09 +0900

    upload

--- DoItC++/06.Greedy/01.Greedy/035/035/035.cpp
+++ DoItC++/06.Greedy/01.Greedy/035/035/035.cpp
@@ -9,48 +9,37 @@ struct Data
 {
 	int _start;
 	int _end;
+	bool _use = false;
 };
-
-bool compareStart(Data a, Data b) { return a._start < b._start; }
-bool compareEnd(Data a, Data b) { return a._end < b._end; }
+bool compare(Data* a, Data* b) 
+{ 
+	if(a->_end == b->_end)
+		return a->_start < b->_start; 
+	return a->_end < b->_end;
+}
 
 int main()
 {
 	int N;
 	scanf("%d", &N);
-	vector<Data> starts(N);
-	vector<Data> ends(N);
+	vector<Data*> datas(N);
 	for (int i = 0; i < N; i++)
 	{
-		scanf("%d", &starts[i]._start);
-		scanf("%d", &starts[i]._end);
-		ends[i]._start = starts[i]._start;
-		ends[i]._end = starts[i]._end;
+		Data* data = new Data;
+		scanf("%d", &data->_start);
+		scanf("%d", &data->_end);
+		datas[i] = data;
 	}
+	sort(datas.begin(), datas.end(), compare);
 
-	sort(starts.begin(), starts.end(), compareStart);
-	sort(ends.begin(), ends.end(), compareEnd);
-
-	int max = pow(2, 31) - 1;
 	int cnt = 1;
-	Data data = ends[0];
-	for (int i = 0; i < N; i++)
+	Data* data = datas[0];
+
+	for (int i = 1; i < N; i++)
 	{
-		if (data._end <= starts[i]._start)
-		{						
-			int idx = -1;
-			int min = max;
-			for (int j = i; j < N; j++)
-			{
-				if (min >= starts[j]._end) 
-				{
-					min = starts[j]._end;
-					idx = j;
-				}
-			}
-			
-			if (idx == -1) break;
-			data = starts[idx];
+		if (data->_end <= datas[i]->_start)
+		{					
+			data = datas[i];
 			cnt++;
 		}
 	}
---
+++ DoItC++/06.Greedy/01.Greedy/036/036/036.cpp
@@ -0,0 +1,47 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <string>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    vector<string> v_str;
+    vector<int> v_int;
+    string input;
+    cin >> input;
+
+    int prev = 0;
+    int cur = input.find('-');
+    while (cur != string::npos) 
+    {
+        string sub = input.substr(prev, cur - prev);
+        v_str.push_back(sub);
+        prev = cur + 1;
+        cur = input.find('-', prev);
+    }
+    v_str.push_back(input.substr(prev, cur - prev));
+
+    int result = 0;
+    v_int.resize(v_str.size(), 0);
+    for (int i = 0; i < v_str.size(); i++)
+    {
+        string data = v_str[i];
+        prev = 0;
+        cur = data.find('+');
+        while (cur != string::npos) 
+        {
+            string sub = data.substr(prev, cur - prev);
+            v_int[i] += stoi(sub);
+            prev = cur + 1;
+            cur = data.find('+', prev);
+        }
+        v_int[i] += stoi(data.substr(prev, cur - prev));
+
+        if (i == 0) result += v_int[i];
+        else result -= v_int[i];
+    }
+
+    ::printf("%d", result);
+    return 0;
+}
